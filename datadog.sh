80527992788c7a1d2e403284eeaaf6d6729a73ab
abd03ujm <89288473+abd03ujm@users.noreply.github.com>
2025-03-12 02:51:33 +0530

    Update datadog.sh

--- datadog.sh
+++ datadog.sh
@@ -1,10 +1,12 @@
-OS_VERSION=$(awk -F= '/^PRETTY_NAME/ {print $2}' /etc/os-release | tr -d '"')
-NODE_VERSION=$(node -v)
+#!/bin/bash
+
+OS_VERSION="3.21"
+NODE_VERSION="18.20"
 
 echo "OS Version: $OS_VERSION"
 echo "Node.js Version: $NODE_VERSION"
 
-# Send data to Datadog with multi-line JSON formatting
+# Send data to Datadog with multi-line JSON formatting and ddsource
 curl -X POST "https://http-intake.logs.datadoghq.com/v1/input" \
      -H "Content-Type: application/json" \
      -H "DD-API-KEY: YOUR_DATADOG_API_KEY" \
@@ -13,6 +15,7 @@ curl -X POST "https://http-intake.logs.datadoghq.com/v1/input" \
   "message": "System Information",
   "os_version": "$OS_VERSION",
   "node_version": "$NODE_VERSION",
-  "service": "my-app"
+  "service": "my-app",
+  "ddsource": "system-metrics"
 }
 EOF
